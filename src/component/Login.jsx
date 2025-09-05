1a7a1900fa14c843c0092a60e8546a6be0d76035
rafika khatun <rafikakhatun607@gmail.com>
2025-09-05 21:11:31 +0530

    feat: add username input field with icon

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,35 +2,38 @@ import { CiUser } from "react-icons/ci";
 import loginImage from "../assets/loginImage.png"
 import { RiLockPasswordLine } from "react-icons/ri";
 import { MdOutlineEmail } from "react-icons/md";
+import { FaEnvelope, FaUser } from "react-icons/fa";
 
 const Login = () => {
 
     return (
 
-        <section className='w-full bg-gray-100 flex flex-col-reverse md:flex-row justify-around items-center px-10 py-12'>
+        <section className='w-full h-screen bg-gray-100 flex flex-col-reverse md:flex-row justify-around items-center px-10 py-12'>
             <form className="p-8 max-w-xl w-full space-y-4">
                 <h1 className="text-center mb-20 text-2xl md:text-4xl font-semibold  bg-gradient-to-r from-pink-600  to-purple-600 bg-clip-text text-transparent">Login to continue Learning</h1>
                 {/*User name field */}
-                <div className="flex">
-                    <CiUser />
-                    <input className="flex-1" type="text" placeholder="Enter your Name" name="userName" />
+                <div className="flex items-center border border-gray-300 rounded-full shadow-md">
+                    <div className="bg-gradient-to-b py-3.5 px-4 from-pink-600  to-purple-600  rounded-l-full flex items-center justify-center text-white">
+                        <FaUser className="w-4 h-4" />
+                    </div>
+                    <input className="w-full py-2 px-6 outline-none rounded-r-full placeholder:text-gray-600" type="text" placeholder="Enter your Name" name="userName" />
                 </div>
                 {/*Email field*/}
-                <div className="flex">
+                <div className="flex items-center gap-2">
                     <MdOutlineEmail />
-                    <input className="flex-1" type="email" placeholder="Enter your Email" name="email" />
+                    <input className="w-full" type="email" placeholder="Enter your Email" name="email" />
                 </div>
 
                 {/*password field*/}
-                <div className="flex">
+                <div className="flex items-center gap-2">
                     <RiLockPasswordLine />
-                    <input className="flex-1" type="password" placeholder="Enter Your Password" name="password" />
+                    <input className="w-full" type="password" placeholder="Enter Your Password" name="password" />
                 </div>
 
 
 
             </form>
-            <div className="p-8">
+            <div className="p-8 hidden md:flex">
                 <img className="max-w-50 md:max-w-70 " src={loginImage} alt="photo" />
 
             </div>
