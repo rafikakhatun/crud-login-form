76133ba4a945035ad107b9d69a679acea53a8379
rafika khatun <rafikakhatun607@gmail.com>
2025-09-05 22:18:55 +0530

    Add social login section google and facebook

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,13 +2,15 @@ import { CiUser } from "react-icons/ci";
 import loginImage from "../assets/loginImage.png"
 import { RiLockPasswordLine } from "react-icons/ri";
 import { MdOutlineEmail } from "react-icons/md";
-import { FaEnvelope, FaUser } from "react-icons/fa";
+import { FaEnvelope, FaFacebook, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
 
     return (
 
-        <section className='w-full h-screen bg-gray-100 flex flex-col-reverse md:flex-row justify-around items-center px-10 py-12'>
+        <section className='w-full h-auto bg-gray-100 flex flex-col-reverse md:flex-row justify-around items-center px-10 py-16'>
             <form className="p-8 max-w-xl w-full space-y-6">
                 <h1 className="text-center mb-20 text-2xl md:text-4xl font-semibold  bg-gradient-to-r from-pink-600  to-purple-600 bg-clip-text text-transparent">Login to continue Learning</h1>
                 {/*User name field */}
@@ -46,6 +48,25 @@ const Login = () => {
 
                 {/*button field */}
                 <button className="w-full px-4 py-2 bg-gradient-to-l from-purple-500 to-pink-600 rounded-full text-white font-semibold">Login</button>
+
+                {/*Register*/}
+                <div className="text-center">
+                    <span className="tracking-wide text-gray-700">Don't Have An Accont? <Link to="" className="font-semibold text-purple-600">Register</Link></span>
+                </div>
+
+                {/*login others account */}
+                <div className="w-full flex items-center justify-center gap-3">
+                    <span className="w-1/3 border-t border-gray-700" ></span>
+                    <p className="text-gray-700 text-sm">Or, Login</p>
+                    <span className="w-1/3 border-t border-gray-700"></span>
+
+                </div>
+
+                {/*google and facebook account field */}
+                <div className="flex justify-center space-x-5 ">
+                    <button className="flex justify-center gap-2 cursor-pointer w-1/2 px-4 py-3 rounded-full border border-gray-300 shadow-md "><FcGoogle className="text-2xl" />Google</button>
+                    <button className="flex justify-center gap-2 cursor-pointer w-1/2 px-4 py-3 rounded-full border border-gray-300 shadow-md  "><FaFacebook className="text-2xl text-blue-600" />Facebook</button>
+                </div>
 
 
 
